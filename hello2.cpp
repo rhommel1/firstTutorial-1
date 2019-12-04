cfef9ea7946ebd8ca5d003a5802a2ad3d3af7d67
Daniel Floryan <danielfloryan@Daniels-MacBook-Pro.local>
2019-12-04 15:43:31 -0600

    Anotha one

---
+++ hello2.cpp
@@ -0,0 +1,14 @@
+//
+//  hello.cpp
+//  
+//
+//  Created by Daniel Floryan on 12/4/19.
+//
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello, world" << endl;
+    cout << "Goodbye, moon" << endl;
+}
